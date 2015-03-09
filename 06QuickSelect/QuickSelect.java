085e16e62d853dd09fc4b0b2bfbd6315aafe398c
roddajohn <johnrod.john@gmail.com>
2015-03-09 12:21:05 -0400

    Small updates

--- 06QuickSelect/QuickSelect.java
+++ 06QuickSelect/QuickSelect.java
@@ -1,8 +1,7 @@
 public class QuickSelect {
     public static void main(String[] args) {
-	int[] toPass = {4, 7, 6, 1, 2, 3, 4};
-	int[] toReturn = partition(toPass, 0, 4);
-	printArr(toReturn);
+	int[] toPass = {-50, -100, 6, 1, 2, 3, 4, -100, -20};
+        quickSelect(toPass, 0, toPass.length - 1, 4);
     }
     public static void printArr (int[] toReturn) {
 	System.out.print("[");
@@ -11,25 +10,72 @@ public class QuickSelect {
 	}
 	System.out.println("]");
     }
-    public static int[] partition(int[] array, int start, int end) {
-	int originalEnd = end;
+    public static void partition(int[] array, int start, int end) {
 	int[] l = new int[array.length];
-	for (int i = 0; i < start; i++) {
-	    l[i] = array[i];
-	}
-	int pivotValue = array[start];
-	for (int i = start + 1; i < originalEnd + 1; i++) {
-	    if (array[i] < pivotValue) {
-		l[start++] = array[i];
-	    }
-	    else {
-		l[end--] = array[i];
+	for (int i = 0; i < array.length; i++) {
+	    if (i < start || i > end) {
+		l[i] = array[i];
 	    }
 	}
-	l[start] = pivotValue;
-	for (int i = originalEnd; i + 1 < array.length; i++) {
-	    l[i + 1] = array[i + 1];
+	int pivot = start + (int) (Math.random() * (end - start + 1));
+	int value = array[pivot];
+	int ei = end;
+	System.out.println("Pivot: " + value);
+	for (int i = start; i <= ei; i++) {
+	    printArr(l);
+	    if (i != pivot) {
+		if (array[i] < value) {
+		    l[start++] = array[i];
+		}
+		else {
+		    l[end--] = array[i];
+		}
+	    }
 	}
-	return l;
+	l[start] = value;
+	printArr(l);
+    }
+
+    public static void swap(int[] array, int i, int j) {
+	int temp = array[i];
+	array[i] = array[j];
+	array[j] = temp;
+    }
+
+
+    public static void quickSelect(int[] array, int start, int end, int n) {
+	int[] l = new int[array.length];
+	for (int i = 0; i < array.length; i++) {
+	    if (i < start || i > end) {
+		l[i] = array[i];
+	    }
+	}
+	int pivot = start + (int) (Math.random() * (end - start + 1));
+	int value = array[pivot];
+	int ei = end;
+	int si = start;
+	System.out.println("Pivot: " + value);
+	for (int i = start; i <= ei; i++) {
+	    printArr(l);
+	    if (i != pivot) {
+		if (array[i] < value) {
+		    l[start++] = array[i];
+		}
+		else {
+		    l[end--] = array[i];
+		}
+	    }
+	}
+	l[start] = value;
+	printArr(l);
+	if (start == n - 1) {
+	    System.out.println(l[start]);
+	}
+	else if (n - 1 < start) {
+	    quickSelect(l, si, start - 1, n);
+	}
+	else {
+	    quickSelect(l, start + 1, ei, n);
+	}
     }
 }
