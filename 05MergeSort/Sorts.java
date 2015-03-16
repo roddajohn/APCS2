1af6ab512bb1617bd154fc6203363a364e525eee
roddajohn <johnrod.john@gmail.com>
2015-03-16 12:19:03 -0400

    LOLOLOLOLOL

--- 05MergeSort/Sorts.java
+++ 05MergeSort/Sorts.java
@@ -1,49 +1,45 @@
-public class MergeSort {
+public class Sorts {
      
     private int[] array;
     private int[] tempMergArr;
     private int length;
 
-    public static String() {
+    public static String name() {
 	return "john.rodda";
     }
  
     public static void main(String a[]){
-         
-        int[] inputArr = {45,23,11,89,77,98,4,28,65,43};
-        MergeSort mms = new MergeSort();
-        mms.sort(inputArr);
-        for(int i:inputArr){
-            System.out.print(i);
-            System.out.print(" ");
-        }
+	int[] toSort = {3, 4, 6, 2, 4, 8, 4, 6, 5, 3};
+	toSort = sort(toSort);
+	for (int i = 0; i < toSort.length; i++) {
+	    System.out.println(" " + toSort[i] + " ");
+	}
+
     }
      
-    public void sort(int inputArr[]) {
-        this.array = inputArr;
-        this.length = inputArr.length;
-        this.tempMergArr = new int[length];
-        doMergeSort(0, length - 1);
+    public static int[] sort(int inputArr[]) {
+        int[] array = inputArr;
+        int length = inputArr.length;
+        int[] tempMergArr = new int[length];
+        doMergeSort(array, length, tempMergArr, 0, length - 1);
+	return array;
     }
     
-    public String name() {
-	return "john.rodda";
-    }
     
-    private void doMergeSort(int lowerIndex, int higherIndex) {
-         
+    private static int[] doMergeSort(int[] array, int length, int[] tempMergArr, int lowerIndex, int higherIndex) {
         if (lowerIndex < higherIndex) {
             int middle = lowerIndex + (higherIndex - lowerIndex) / 2;
             // Below step sorts the left side of the array
-            doMergeSort(lowerIndex, middle);
+            doMergeSort(array, length, tempMergArr, lowerIndex, middle);
             // Below step sorts the right side of the array
-            doMergeSort(middle + 1, higherIndex);
+            doMergeSort(array, length, tempMergArr, middle + 1, higherIndex);
             // Now merge both sides
-            mergeParts(lowerIndex, middle, higherIndex);
+            mergeParts(array, length, tempMergArr, lowerIndex, middle, higherIndex);
         }
+	return array;
     }
  
-    private void mergeParts(int lowerIndex, int middle, int higherIndex) {
+    private static int[] mergeParts(int[] array, int length, int[] tempMergArr, int lowerIndex, int middle, int higherIndex) {
         for (int i = lowerIndex; i <= higherIndex; i++) {
             tempMergArr[i] = array[i];
         }
@@ -65,5 +61,6 @@ public class MergeSort {
             k++;
             i++;
         }
+	return array;
     }
 }
